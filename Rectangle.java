2752f52cb3cb4688c1bfd2e1baca8281c4d878e1
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-12 19:32:42 +0300

    Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,34 @@
+// Rectangle.java
+public class Rectangle implements Shape {
+    private double width;
+    private double height;
+    private String fillColor;
+    private String borderColor;
+
+    public Rectangle(double width, double height, String fillColor, String borderColor) {
+        this.width = width;
+        this.height = height;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;  // Площадь прямоугольника
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (width + height);  // Периметр прямоугольника
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
