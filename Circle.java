d5d96c4f1d8e3c48225ff67359d72d7cd6fe9d83
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-12 19:28:22 +0300

    Circle.java

---
+++ Circle.java
@@ -0,0 +1,32 @@
+// Circle.java
+public class Circle implements Shape {
+    private double radius;
+    private String fillColor;
+    private String borderColor;
+
+    public Circle(double radius, String fillColor, String borderColor) {
+        this.radius = radius;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double calculateArea() {
+        return  radius  radius;  // Площадь круга
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return   radius;  // Периметр круга
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
