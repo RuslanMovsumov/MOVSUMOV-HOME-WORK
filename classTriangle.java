59456b8940e2d304e078414756ba9d1e64294a1c
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-15 10:26:36 +0300

    classTriangle.java

--- classTriangle.java
+++ classTriangle.java
@@ -3,7 +3,7 @@ class Triangle.java extends Shape {
    private double b;
    private double c;
 
-   public Triangle.java (double a, double b, double c, String fillColor, String borderColor) {
+   public Triangle(double a, double b, double c, String fillColor, String borderColor) {
       super(fillColor, borderColor);
       this.a = a;
       this.b = b;
