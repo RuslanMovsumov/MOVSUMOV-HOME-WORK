f42d1aad2f9c3480534afd04432822069b7063d7
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-16 11:12:06 +0300

    processArray.java

---
+++ processArray.java
@@ -0,0 +1,20 @@
+public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Неверный размер массива. Ожидается 4x4.");
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Невозможно преобразовать элемент в число.", i, j);
+                }
+            }
+        }
+
+        return sum;
+    }
+}
