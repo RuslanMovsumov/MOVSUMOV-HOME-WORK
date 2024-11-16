4ba5f62478796408f920849a94e3d029ebbfaddb
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-16 11:12:41 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -42,25 +42,4 @@ public class Main {
                 System.out.println("Ошибка в ячейке: " + dataException.getRow() + ", " + dataException.getCol());
             }
         }
-    }
-
-    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
-        if (array.length != 4 || array[0].length != 4) {
-            throw new MyArraySizeException("Неверный размер массива. Ожидается 4x4.");
-        }
-
-        int sum = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                try {
-                    sum += Integer.parseInt(array[i][j]);
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("Невозможно преобразовать элемент в число.", i, j);
-                }
-            }
-        }
-
-        return sum;
-    }
-}
+    
