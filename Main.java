b20630692b88815cca9dc48ef33cf80624232b73
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-17 20:47:22 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,45 +1,27 @@
-public class Main {
-
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         String[][] validArray = {
-                {"1", "2", "3", "4"},
-                {"5", "6", "7", "8"},
-                {"9", "10", "11", "12"},
-                {"13", "14", "15", "16"}
+            {"1", "2", "3", "4"},
+            {"5", "6", "7", "8"},
+            {"9", "10", "11", "12"},
+            {"13", "14", "15", "16"}
         };
 
-        String[][] invalidSizeArray = {
-                {"1", "2", "3"},
-                {"4", "5", "6"}
+        String[][] invalidArraySize = {
+            {"1", "2"},
+            {"3", "4"}
         };
 
-        String[][] invalidDataArray = {
-                {"1", "2", "3", "4"},
-                {"5", "6", "A", "8"},
-                {"9", "10", "11", "12"},
-                {"13", "14", "15", "16"}
+        String[][] invalidArrayData = {
+            {"1", "2", "3", "a"},
+            {"5", "6", "7", "8"},
+            {"9", "10", "11", "12"},
+            {"13", "14", "15", "16"}
         };
 
         try {
-            int result = processArray(validArray);
-            System.out.println("Сумма элементов: " + result);
-        } catch (MyArraySizeException | MyArrayDataException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
+            System.out.println("Сумма элементов: " + sumArray(validArray));
 
-        try {
-            processArray(invalidSizeArray);
         } catch (MyArraySizeException | MyArrayDataException e) {
-            System.out.println("Ошибка: " + e.getMessage());
+            System.err.println(e.getMessage());
         }
-
-        try {
-            processArray(invalidDataArray);
-        } catch (MyArraySizeException | MyArrayDataException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-            if (e instanceof MyArrayDataException) {
-                MyArrayDataException dataException = (MyArrayDataException) e;
-                System.out.println("Ошибка в ячейке: " + dataException.getRow() + ", " + dataException.getCol());
-            }
-        }
-    
+    }
