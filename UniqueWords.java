28295205a12b68f305c69bbd16b9310c6dc2d7ee
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-21 15:45:45 +0300

    UniqueWords.java

--- UniqueWords.java
+++ UniqueWords.java
@@ -1,24 +1,23 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-public class UniqueWords {
+public class UniqueWordsExample {
     public static void main(String[] args) {
         String[] words = {
-        "банан", "яблоко", "виноград", "апельсин", "банан",
-        "яблоко", "гранат", "виноград", "апельсин", "банан",
-        "груша", "груша", "яблоко", "гранат", "апельсин"
+            "банан", "яблоко", "груша", "яблоко", "виноград", 
+            "груша", "персик", "банан", "яблоко", "вишня", 
+            "апельсин", "виноград", "груша", "яблоко", "груша"
         };
 
-        // Получаем список новых уникальных слов 
-        Set<String> uniqueWords = new HashSet<>(Arrays.asList(words));
-
-        // Подсчитываем количество вхождений каждого слова
+        // Используем HashMap для подсчета частоты слов
         Map<String, Integer> wordCount = new HashMap<>();
+
         for (String word : words) {
             wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         }
 
-        System.out.println("Уникальные слова: " + uniqueWords);
-        System.out.println("Количество вхождений слов:");
+        // Выводим уникальные слова и их частоту
+        System.out.println("Уникальные слова и их частота:");
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
