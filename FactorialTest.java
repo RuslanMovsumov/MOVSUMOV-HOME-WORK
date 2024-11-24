a69ecf9f899248f0ed5b5f53402d1b7ad039dc38
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-24 19:27:14 +0300

    FactorialTest.java

---
+++ FactorialTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FactorialTest {
+
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(1, Factorial.calculateFactorial(0));
+    }
+
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        assertEquals(120, Factorial.calculateFactorial(5));
+    }
+
+    @Test
+    public void testFactorialOfNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Factorial.calculateFactorial(-1);
+        });
+    }
+}
+
