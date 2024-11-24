29a8ec41aa729cff25201f73d372c892ba33da8d
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-24 19:49:20 +0300

    FactorialTestNG.java

--- FactorialTestNG.java
+++ FactorialTestNG.java
@@ -6,18 +6,23 @@ public class FactorialTestNG {
 
     @Test
     public void testFactorialOfZero() {
-        assertEquals(Factorial.calculateFactorial(0), 1);
+        assertEquals(Factorial.calculateFactorial(0), 1, "Факториал нуля должен быть 1");
     }
 
     @Test
     public void testFactorialOfPositiveNumber() {
-        assertEquals(Factorial.calculateFactorial(5), 120);
+        assertEquals(Factorial.calculateFactorial(5), 120, "Факториал 5 должен быть 120");
     }
 
+    @Test
+    public void testFactorialOfLargeNumber() {
+        assertEquals(Factorial.calculateFactorial(10), 3628800, "Факториал 10 должен быть 3628800");
+    }
+    
     @Test
     public void testFactorialOfNegativeNumber() {
         assertThrows(IllegalArgumentException.class, () -> {
             Factorial.calculateFactorial(-1);
-        });
+        }, "Факториал отрицательного числа должен выбрасывать IllegalArgumentException");
     }
 }
