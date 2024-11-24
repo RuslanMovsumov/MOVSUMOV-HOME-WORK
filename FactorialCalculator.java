63f2cfa55bf31884ee9eaec2b90159521ccaf453
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-24 19:26:10 +0300

    FactorialCalculator.java

--- FactorialCalculator.java
+++ FactorialCalculator.java
@@ -10,8 +10,4 @@ public class FactorialCalculator {
         return result;
     }
 
-    public static void main(String[] args) {
-        // Пример использования
-        System.out.println("Факториал 5: " + factorial(5)); // Должно вывести 120
-    }
-}
+    
