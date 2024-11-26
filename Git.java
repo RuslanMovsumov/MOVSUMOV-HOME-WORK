7ca119d225c9f20cf499e4c096f0df91f4348110
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-27 01:05:14 +0300

    Git.java

--- Git.java
+++ Git.java
@@ -1,3 +1,4 @@
+"Добавлен класс для вычисления факториала и юнит-тесты с использованием JUnit 5"
 git add Factorial.java 
 FactorialTestNG.java
-git commit -m "Добавлена программа для вычисления факториала и тесты NG"
+git commit -m 
