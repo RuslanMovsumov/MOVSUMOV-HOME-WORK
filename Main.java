859175f3eb3b87b3b97035aa5103f958713a5aa2
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-26 23:28:52 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,19 @@
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         String[][] validArray = {
             {"1", "2", "3", "4"},
             {"5", "6", "7", "8"},
             {"9", "10", "11", "12"},
             {"13", "14", "15", "16"}
         };
-
-        String[][] invalidArraySize = {
-            {"1", "2"},
-            {"3", "4"}
-        };
-
-        String[][] invalidArrayData = {
-            {"1", "2", "3", "a"},
-            {"5", "6", "7", "8"},
-            {"9", "10", "11", "12"},
-            {"13", "14", "15", "16"}
-        };
-
-       // Проверка валидного массива
+        
         try {
-            System.out.println("Сумма элементов: " + sumArray(validArray));
-       } catch (MyArraySizeException | MyArrayDataException e) {
+            int sum = ArraySum.calculateArraySum(validArray);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException e) {
             System.err.println(e.getMessage());
-        }
-
-      // Проверка массива с некорректным размером
-        try {
-            System.out.println("Сумма элементов (некорректный размер): " + calculateArraySum(invalidArraySize));
-        } catch (MyArraySizeException | MyArrayDataException e) {
-            System.err.println(e.getMessage());
-        }
-
-        // Проверка массива с некорректными данными
-        try {
-            System.out.println("Сумма элементов (некорректные данные): " + calculateArraySum(invalidArrayData));
-        } catch (MyArraySizeException | MyArrayDataException e) {
+        } catch (MyArrayDataException e) {
             System.err.println(e.getMessage());
         }
     }
+}
