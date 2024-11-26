29ae1a7dfca06c3529cf53d2f6dc01fc433ebc32
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-27 00:31:32 +0300

    Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,7 @@
 public class Factorial {
     public static long calculateFactorial(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("Число должно быть неотрицательным");
+            throw new IllegalArgumentException("Факториал не определен для отрицательных чисел");
         }
         long result = 1;
         for (int i = 1; i <= n; i++) {
