7669cf12ac9666eb4861025d45c4deb53efeb1f3
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-27 00:45:56 +0300

    FactorialTest.java

---
+++ FactorialTest.java
@@ -0,0 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FactorialTest {
+    
+    @Test
+    public void testFactorialOfZero() {
+        Assert.assertEquals(Factorial.calculateFactorial(0), 1);
+    }
+    
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        Assert.assertEquals(Factorial.calculateFactorial(5), 120);
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        Factorial.calculateFactorial(-1);
+    }
+}
