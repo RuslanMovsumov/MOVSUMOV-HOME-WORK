ffae371dceb73e8a0cc8f5926f03818b244db234
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-26 23:26:29 +0300

    ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -1,10 +1,10 @@
 public class ArraySum {
-    
+
     public static int calculateArraySum(String[][] array) throws MyArraySizeException, MyArrayDataException {
         if (array.length != 4 || array[0].length != 4) {
             throw new MyArraySizeException("Размер массива должен быть 4x4.");
         }
-        
+
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
@@ -14,5 +14,8 @@ public class ArraySum {
                     throw new MyArrayDataException("Ошибка преобразования в число на позиции [" + i + "][" + j + "]: " + array[i][j]);
                 }
             }
-          return sum;
+        }
+        return sum;
     }
+}
+
