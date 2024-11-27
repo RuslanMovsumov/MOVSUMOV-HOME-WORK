53260109ea0f5f959737cbb0d36c5d6c872f60b8
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-27 12:31:14 +0300

    Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,12 @@
+package com.example;
+
 public class Factorial {
     public static long calculateFactorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Число не должно быть отрицательным");
+        }
+        if (n == 0) {
+            return 1;
         }
         long result = 1;
         for (int i = 1; i <= n; i++) {
@@ -10,5 +15,3 @@ public class Factorial {
         return result;
     }
 }
-
-    
