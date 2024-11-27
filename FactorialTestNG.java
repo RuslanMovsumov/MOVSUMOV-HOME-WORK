5990a96ae53563f018dbb914725eadb9c471015f
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-27 12:34:52 +0300

    FactorialTestNG.java

--- FactorialTestNG.java
+++ FactorialTestNG.java
@@ -1,18 +1,20 @@
-import org.testng.Assert;
+package com.example;
+
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class FactorialTestNG {
-    
+
     @Test
     public void testFactorialOfZero() {
-        Assert.assertEquals(Factorial.calculateFactorial(0), 1);
+        assertEquals(Factorial.calculateFactorial(0), 1);
     }
-    
+
     @Test
     public void testFactorialOfPositiveNumber() {
-        Assert.assertEquals(Factorial.calculateFactorial(5), 120);
+        assertEquals(Factorial.calculateFactorial(5), 120);
     }
-    
+
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testFactorialOfNegativeNumber() {
         Factorial.calculateFactorial(-1);
