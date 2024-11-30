b28d1c44d64232624e2b9964930b70e7fb50dff6
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-30 21:55:43 +0300

    Delete checkEmptyFieldLabels.java

--- checkEmptyFieldLabels.java
+++
@@ -1,6 +0,0 @@
-public void checkEmptyFieldLabels() {
-    assert connectionServicesField.getAttribute("placeholder").equals("Введите услуги связи");
-    assert homeInternetField.getAttribute("placeholder").equals("Введите домашний интернет");
-    assert installmentField.getAttribute("placeholder").equals("Введите рассрочку");
-    assert debtField.getAttribute("placeholder").equals("Введите задолженность");
-}
