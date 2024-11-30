8bde64d288ea28a28e4657a062f6cfd68eb1c930
Movsumov Ruslan <movsumov939@gmail.com>
2024-11-30 21:57:21 +0300

    OnlineRechargeTest.java

---
+++ OnlineRechargeTest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OnlineRechargeTest {
+    private WebDriver driver;
+    private OnlineRechargePage onlineRechargePage;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("URL_вашего_сайта");  // Замените на реальный URL
+        onlineRechargePage = new OnlineRechargePage(driver);
+    }
+
+    @Test
+    public void testOnlineRechargePage() {
+        onlineRechargePage.checkEmptyFieldLabels();
+        onlineRechargePage.fillConnectionServices("1234567890");
+        onlineRechargePage.verifyDetailsAfterContinue("Ожидаемая сумма");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
