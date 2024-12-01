a39449c64e5e3ffd5f5763d7bbc6ebda58614f6d
Movsumov Ruslan <movsumov939@gmail.com>
2024-12-01 16:29:07 +0300

    OnlineRechargeTest.java

--- src/main/java/com/example/tests/OnlineRechargeTest.java
+++ src/main/java/com/example/tests/OnlineRechargeTest.java
@@ -3,6 +3,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.junit.Assert.assertTrue;
 
 public class OnlineRechargeTest {
     private WebDriver driver;
@@ -10,20 +13,38 @@ public class OnlineRechargeTest {
 
     @Before
     public void setUp() {
+        // Устанавливаем драйвер браузера
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize(); 
         driver.get("https://www.mts.by/");  
         onlineRechargePage = new OnlineRechargePage(driver);
     }
 
     @Test
     public void testOnlineRechargePage() {
+        // Проверяем названия пустых полей
         onlineRechargePage.checkEmptyFieldLabels();
+
+        // Заполняем информацию и нажимаем кнопку "Продолжить"
         onlineRechargePage.fillConnectionServices("1234567890");
-        onlineRechargePage.verifyDetailsAfterContinue("Ожидаемая сумма");
+
+        // Замените "Ожидаемая сумма" на правильное значение, которое ваше приложение должно отображать
+        String expectedAmount = "Ожидаемая сумма"; 
+        String expectedPhoneNumber = "297777777"; 
+
+        // Проверяем результаты после нажатия кнопки "Продолжить"
+        onlineRechargePage.verifyDetailsAfterContinue(expectedAmount, expectedPhoneNumber);
+        
+        // Дополнительная проверка, чтобы убедиться, что иконки платежных систем отображаются
+        assertTrue("Иконки платежных систем отображаются.", onlineRechargePage.getPaymentSystemIcons().size() > 0);
     }
 
     @After
     public void tearDown() {
-        driver.quit();
+        // Закрываем браузер
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
