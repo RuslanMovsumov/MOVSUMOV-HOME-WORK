82349f505616b16cf84719ed656858abddc62b93
Movsumov Ruslan <movsumov939@gmail.com>
2024-12-03 20:18:59 +0300

    OnlineRechargePage.java

--- src/main/java/com/example/pageobjects/OnlineRechargePage.java
+++ src/main/java/com/example/pageobjects/OnlineRechargePage.java
@@ -1,8 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import java.util.List;
 
 public class OnlineRechargePage {
@@ -50,9 +50,9 @@ public class OnlineRechargePage {
         continueButton.click();
     }
 
-    public void verifyDetailsAfterContinue(String expectedAmount) {
+    public void verifyDetailsAfterContinue(String expectedAmount, String expectedPhoneNumber) {
         assert driver.findElement(By.id("amount_display")).getText().equals(expectedAmount);
-        assert phoneNumberField.getAttribute("value").equals("номер телефона");
+        assert phoneNumberField.getAttribute("value").equals(expectedPhoneNumber);
         assert cardNumberField.getAttribute("placeholder").equals("Введите номер карты");
         assert paymentSystemIcons.size() > 0;
     }
