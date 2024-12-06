a21a08d98ad9a8cc2569ff9bbdd72c6ec9be94b2
Movsumov Ruslan <movsumov939@gmail.com>
2024-12-06 11:04:53 +0300

    project.java

---
+++ project.java
@@ -0,0 +1,13 @@
+project
+├── .idea (в gitignore)
+├── src
+│   ├── main
+│   │   └── java
+│   │       └── pageobjects
+│   │           ├── OnlineRechargePage.java
+│   │           └── BasePage.java
+│   └── tests
+│       └── java
+│           └── tests
+│               └── MTSOnlineRechargeTests.java
+└── pom.xml
