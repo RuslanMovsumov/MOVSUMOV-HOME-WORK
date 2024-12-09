9e3b9c6f6b11c101ad1866197534ba989922f267
Movsumov Ruslan <movsumov939@gmail.com>
2024-12-09 21:51:45 +0300

    OnlineRechargeTest.java

--- src/main/java/com/example/tests/OnlineRechargeTest.java
+++ src/main/java/com/example/tests/OnlineRechargeTest.java
@@ -5,6 +5,11 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 public class OnlineRechargePageTest {
     private WebDriver driver;
     private OnlineRechargePage onlineRechargePage;
@@ -18,34 +23,37 @@ public class OnlineRechargePageTest {
     }
 
     @Test
-public void testEmptyFieldPlaceholders() {
-    onlineRechargePage.selectServiceType("Услуги связи");
-    onlineRechargePage.enterPhoneNumber(""); 
-    onlineRechargePage.clickContinueButton();
+    public void testEmptyFieldPlaceholders() {
+        onlineRechargePage.selectServiceType("Услуги связи");
+        onlineRechargePage.enterPhoneNumber("");
+        onlineRechargePage.clickContinueButton();
 
-    assertEquals("Введите номер карты", onlineRechargePage.getEmptyFieldPlaceholder()); 
-}
+        assertEquals("Введите номер карты", onlineRechargePage.getEmptyFieldPlaceholder());
+    }
 
     @Test
-public void testServicePaymentDetails() {
-    onlineRechargePage.selectServiceType("Услуги связи");
-    onlineRechargePage.enterPhoneNumber("297777777");
-    onlineRechargePage.clickContinueButton();
+    public void testServicePaymentDetails() {
+        onlineRechargePage.selectServiceType("Услуги связи");
+        onlineRechargePage.enterPhoneNumber("297777777");
+        onlineRechargePage.clickContinueButton();
 
-    assertEquals("Ожидаемая сумма", onlineRechargePage.getDisplayedAmount());
-    assertEquals("297777777", onlineRechargePage.getDisplayedPhone());
+        assertEquals("Ожидаемая сумма", onlineRechargePage.getDisplayedAmount());
+        assertEquals("297777777", onlineRechargePage.getDisplayedPhone());
 
-    assertEquals("Введите номер карты", onlineRechargePage.getEmptyFieldPlaceholder());
-    assertEquals("Введите срок действия карты", onlineRechargePage.getEmptyFieldPlaceholder());
-    assertEquals("Введите CVC", onlineRechargePage.getEmptyFieldPlaceholder());
+        // Проверка на наличие плейсхолдеров
+        assertEquals("Введите номер карты", onlineRechargePage.getCardNumberPlaceholder());
+        assertEquals("Введите срок действия карты", onlineRechargePage.getExpirationDatePlaceholder());
+        assertEquals("Введите CVC", onlineRechargePage.getCVCPlaceholder());
 
-    List<String> actualLogos = onlineRechargePage.getPaymentSystemLogos();
-    assertTrue(actualLogos.contains("Visa"));
-    assertTrue(actualLogos.contains("MasterCard"));
-}
+        List<String> actualLogos = onlineRechargePage.getPaymentSystemLogos();
+        assertTrue(actualLogos.contains("Visa"));
+        assertTrue(actualLogos.contains("MasterCard"));
+    }
 
     @AfterClass
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
