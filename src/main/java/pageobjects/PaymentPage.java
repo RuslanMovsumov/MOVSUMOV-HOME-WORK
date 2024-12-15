de05afdd9f8ca277a932cbf15393530810c311e0
Movsumov Ruslan <movsumov939@gmail.com>
2024-12-15 17:51:45 +0300

    PaymentPage.java

--- src/main/java/pageobjects/PaymentPage.java
+++ src/main/java/pageobjects/PaymentPage.java
@@ -2,21 +2,34 @@ package pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PaymentPage extends BasePage {
 
     private By paymentSuccessMessage = By.id("paymentSuccessMessage");
     private By paymentErrorMessage = By.id("paymentErrorMessage");
-    
+
     public PaymentPage(WebDriver driver) {
         super(driver);
     }
 
     public boolean isPaymentSuccessMessageDisplayed() {
-        return driver.findElement(paymentSuccessMessage).isDisplayed();
+        return isElementDisplayed(paymentSuccessMessage);
     }
 
     public boolean isPaymentErrorMessageDisplayed() {
-        return driver.findElement(paymentErrorMessage).isDisplayed();
+        return isElementDisplayed(paymentErrorMessage);
+    }
+
+    private boolean isElementDisplayed(By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return element.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 }
